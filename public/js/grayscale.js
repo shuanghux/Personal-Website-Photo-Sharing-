46f4a9889cde9cb5287be9d0f38e76c33e07c4cc
Shuang Hu <sunwho707@gmail.com>
2017-10-09 00:16:22 +0000

    add logo, mapmarker

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -57,7 +57,7 @@ function init() {
 
     // Disables the default Google Maps UI components
     disableDefaultUI: false,
-    scrollwheel: false,
+    scrollwheel: true,
     draggable: true,
 
     // How you would like to style the map.
